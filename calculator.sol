4b0f1b83db5d762fa959582653e0b84d86981995
Manny Urbino <32224520+murbino@users.noreply.github.com>
2017-10-06 13:02:06 -0400

    calculator

---
+++ calculator.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+
+/* Add two integers and return result */
+contract Calculator {
+   uint addResult;
+
+   function addNumber(uint num1, uint num2) {
+       addResult = num1 + num2;
+   }
+
+   function getResult() constant returns (uint) {
+       return addResult;
+   }
+}
